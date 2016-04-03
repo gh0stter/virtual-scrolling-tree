657002957ea72a5f2e94cb515d14a1daf9c97fd8
unknown <paul.sweeney@ericsson.com>
2016-04-03 21:27:58 +0100

    Changed text module

--- require.config.js
+++ require.config.js
@@ -2,7 +2,7 @@ var require = {
     baseUrl: "src",
     paths: {
         "presenter": "../node_modules/presenter/presenter",
-        "text": "../node_modules/text/text"
+        "text": "../node_modules/requirejs-text/text"
     },
     packages: [
         {name: "virtual-scrolling-tree", main: "VirtualScrollingTree"}
